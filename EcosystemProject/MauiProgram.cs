31ae36de46ffabf31bd49add8df6f8ab2ce41377
radmou99 <mouradm.mm@outlook.com>
2023-01-02 05:23:38 +0100

    ajout reproduct, eat, fuite, herbivores et carnivores classes, méthode globale check entities. Les créatures font ce qu'il faut mais à améliorer

--- EcosystemProject/MauiProgram.cs
+++ EcosystemProject/MauiProgram.cs
@@ -1,18 +1,2 @@
 ﻿namespace EcosystemProject;
 
-public static class MauiProgram
-{
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
-		return builder.Build();
-	}
-}
